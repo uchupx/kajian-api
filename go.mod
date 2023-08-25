7676100e5e5a448d5d4fdca98edcb35fe29eec65
Yusuf Andriyanto <yusufxx33@gmail.com>
2023-08-25 20:31:32 +0700

    Feature(Adaptor): add redis adaptor

--- go.mod
+++ go.mod
@@ -7,9 +7,12 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/redis/go-redis/v9 v9.1.0
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,1 +1,28 @@
 package redis
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisConfig struct {
+	Host     string `yaml:"host"`
+	Password string `yaml:"password"`
+	Database int    `yaml:"database"`
+}
+
+func NewRedisConn(c RedisConfig) (*redis.Client, error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     c.Host,
+		Password: c.Password,
+		DB:       c.Database,
+	})
+
+	if err := redisClient.Ping(context.TODO()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to ping redis: %w", err)
+	}
+
+	return redisClient, nil
+}
